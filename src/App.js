5bb0318e5cea8bb6c5e334d5707f93a87fd67e40
Marcos Bazbih <marcosbaza100@gmail.com>
2022-02-09 23:46:07 +0200

    main folders added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
-  <p>dd</p>
-  <h1>asdasdasd</h1>
-  <h1>aaaaaaa</h1>
-  <h1>bbbbb</h1>
     </div>
   );
 }
