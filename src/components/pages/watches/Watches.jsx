39d3b923d15dfd0d4f1be0d09b2b4bd3616a7fbb
YafitSamuel <gyafit642@gmail.com>
2022-02-10 03:49:53 +0200

    add comoponents

---
+++ src/components/pages/watches/Watches.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Watches() {
+    
+    return (
+      <div className=" main Watches">
+        <h1> Earring!</h1>
+      </div>
+    );
+  }
+  export default Watches;
+  
