11f06e2a803f89a11ceb80722da084ed879c0ff9
matan-ysayas <matan.ysayas@gmail.com>
2022-02-10 05:48:15 +0200

    aa

--- src/components/pages/rings/Rings.jsx
+++ src/components/pages/rings/Rings.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import RingsCards from"./RingsCards"
 function Rings() {
     
     return (
       <div className=" main Rings">
-       <RingsCards/>
       </div>
     );
   }
