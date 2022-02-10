6eee51c23b9251a48d3c02c3d71ba518fa4b43a1
YafitSamuel <gyafit642@gmail.com>
2022-02-10 02:44:07 +0200

    yafito commit

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  
+    return (
+      <div className=" main Home">
+        <h1>Hello HOME!</h1>
+      </div>
+    );
+  }
+  export default Home;
