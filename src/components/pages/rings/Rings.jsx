bba3bfe706a3ab98f80ea543d35c6a7ab540f528
Marcos Bazbih <marcosbaza100@gmail.com>
2022-02-10 06:54:39 +0200

    cards 50%

--- src/components/pages/rings/Rings.jsx
+++ src/components/pages/rings/Rings.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { RingsContext } from "../../../contexts/rings-context";
+import RingCards from "../../parts/ring-cards/ringCards";
+
 function Rings() {
-    
-    return (
-      <div className=" main Rings">
-      </div>
-    );
-  }
-  export default Rings;
+  const { rings } = useContext(RingsContext);
+
+  return (
+    <div className=" main Rings">
+      {
+        rings.map((item) =>
+          <RingCards item={item} />
+        )
+      }
+    </div>
+  );
+}
+export default Rings;
